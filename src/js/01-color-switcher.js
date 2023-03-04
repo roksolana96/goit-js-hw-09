f82598358ebc24cdd1acee7ca3f348170dc35023
Roksolana96 <roxolana.busko@gmail.com>
2023-03-04 11:55:37 +0100

    hw1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+
+
+let timerId = null;
+isActive = false;
+
+startBtn.addEventListener("click", startBtnColor);
+stopBtn.addEventListener("click", stopBtnColor);
+
+function startBtnColor() {
+  if (isActive) {
+  return;
+};
+  isActive = true;
+    startBtn.classList.add('button-color');
+    stopBtn.classList.remove('button-color');
+
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+
+function stopBtnColor() {
+  isActive = false;
+
+    stopBtn.classList.add('button-color');
+    startBtn.classList.remove('button-color');
+  clearInterval(timerId);
+};
